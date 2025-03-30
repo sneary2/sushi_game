6c2e40fcace41d15fa2860290f19b4c60ef22799
shane <shane@pop-os.lan>
2025-03-30 19:57:09 +0100

    Fixed issue with lock not being released by the main thread for the worker thread to access

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use std::thread::{self, current};
+
 use rand::seq::SliceRandom; // For shuffling
 use rand::thread_rng; // Random number generator
+
+use std::sync::{Arc, Mutex, Condvar};
+// use std::thread;
+
+const NIGIRI_COUNT : u32 = 12;
+const SUSHI_ROLLS_COUNT : u32 = 12;
+const APPETIZERS_COUNT : u32 = 12;
+const SPECIALS_COUNT : u32 = 12;
+const DESSERTS_COUNT : u32 = 12;
+
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -13,21 +25,6 @@ enum Card {
     Tea,
     GreeTeaIceCream
 }
-
-// #[derive(Debug, Clone, PartialEq, Eq)]
-// struct Card {
-//     name: CardType,
-//     id: u32,
-// }
-
-// impl Card {
-//     fn new(name: CardType, id: u32) -> Self {
-//         Self{
-//             name,
-//             id
-//         }
-//     }
-// }
 
 #[allow(non_camel_case_types)]
 #[allow(dead_code)]
@@ -53,20 +50,31 @@ impl Deck {
         let mut deck: Deck = Self{cards: Vec::new()};
         match setup {
             Setup::MY_FIRST_MEAL => {
-                deck.add_card(Card::Nigiri, 5);
-                deck.add_card(Card::Maki, 5);
-                deck.add_card(Card::Tempura, 5);
-                deck.add_card(Card::Sashimi, 5);
-                deck.add_card(Card::MisoSoup, 5);
-                deck.add_card(Card::Wasabi, 5);
-                deck.add_card(Card::Tea, 5);
-                // deck.cards.push(Card::new(Card::GreeTeaIceCream, 1);
+                deck.add_card_to_game(Card::Nigiri);
+                deck.add_card_to_game(Card::Maki);
+                deck.add_card_to_game(Card::Tempura);
+                deck.add_card_to_game(Card::Sashimi);
+                deck.add_card_to_game(Card::MisoSoup);
+                deck.add_card_to_game(Card::Wasabi);
+                deck.add_card_to_game(Card::Tea);
+                // deck.cards.push(Card::new(Card::GreeTeaIceCream);
             }
             _ => {
                 println!("Not supported");
             }
         }
         deck
+    }
+
+    fn add_card_to_game(&mut self, card: Card) {
+        match card {
+            Card::Nigiri                                    => self.add_card(card, NIGIRI_COUNT     ),
+            Card::Maki                                      => self.add_card(card, SUSHI_ROLLS_COUNT),
+            Card::Tempura | Card::Sashimi | Card::MisoSoup  => self.add_card(card, APPETIZERS_COUNT ),
+            Card::Wasabi | Card::Tea                        => self.add_card(card, SPECIALS_COUNT   ),
+            Card::GreeTeaIceCream                           => self.add_card(card, DESSERTS_COUNT   ),
+            // _                                               => println!("Card Not Supported Yet"),
+        }
     }
 
     fn add_card(&mut self, card: Card, count: u32) {
@@ -79,22 +87,90 @@ impl Deck {
         let mut rng = thread_rng();
         self.cards.shuffle(&mut rng)
     }
+
+    fn draw(&mut self, amount : usize) -> Vec<Card> {
+        let mut hand = Vec::with_capacity(9);
+        for _ in 0..amount {
+            hand.push(self.cards.pop().unwrap());
+        }
+        return hand;
+    }
 }
 
 fn main() {
     println!("Start Game!");
     
-    Deck::new(Setup::DINNER_FOR_TWO);
+    // Deck::new(Setup::DINNER_FOR_TWO);
 
-    let mut deck : Deck = Deck::new(Setup::MY_FIRST_MEAL);
+    let deck = Arc::new(Mutex::new(Deck::new(Setup::MY_FIRST_MEAL)));
+    let round: Arc<(Mutex<i32>, Condvar)> = Arc::new((Mutex::new(0), Condvar::new()));
 
-    for card in &deck.cards {
-        println!("Drew card: {:?}", card)
+    // for card in &deck.cards {
+    //     println!("Drew card: {:?}", card)
+    // }
+
+    // deck.shuffle();
+    {
+        deck.lock().unwrap().shuffle();
     }
 
-    deck.shuffle();
+    let mut handles = Vec::new();
 
-    
+    // for player_id in 0..1 {
+    for player_id in 0..=3 {
+
+        // let deck = deck.clone();
+        let round = round.clone();
 
 
+        // let hand: Vec<Card>;
+        // {
+        //     hand = deck.lock().unwrap().draw(9);
+        // } 
+
+        // let field : Vec<Card> = Vec::with_capacity(9);
+
+        handles.push({thread::spawn(move || {
+            
+            let player = player_id;
+            let (turn_lock, cvar) = &*round;
+            // let mut curr_game_round = *turn_lock.lock().unwrap();
+            let mut player_game_round = 1;
+
+            // Wait for the round to start
+            // while current_round != curr_game_round {
+
+            let global_turn: i32 = *turn_lock.lock().unwrap();
+            println!("Player {player}: Internal turn is {player_game_round}, Global turn is {global_turn}");
+
+            let turn_mut = turn_lock.lock().unwrap();
+            while player_game_round != *turn_mut {
+
+                println!("Round is not correct, Player {player} will wait for the round to be updated");
+                // Round is not correct
+                // Wait for the next round
+                // let turn_lock_mut = turn_lock.lock().unwrap();
+                
+                let turn_lock = cvar.wait(turn_mut).unwrap();
+
+                // Update our current value for the round
+                player_game_round = *turn_lock;
+
+                println!("Player {player} has entered Turn {player_game_round}");
+                return;
+            }
+            println!("Player {player} thread is exiting");
+        })});
+    }
+
+    {
+        let (turn_lock, cvar) = &*round;
+        let mut turn = turn_lock.lock().unwrap();
+        *turn += 1;
+        println!("Main: Updated turn to {turn}");
+        cvar.notify_all();
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
