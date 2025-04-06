f52d83010cfc48db1aafbd3b8216bcc188657c32
shane <shane@pop-os.lan>
2025-04-06 08:32:00 +0100

    Cleaned up some code

--- src/game/player.rs
+++ src/game/player.rs
@@ -6,6 +6,7 @@ use crate::game::card::Card;
 pub fn create_player(player_id : usize, turn_barrier: Arc<Barrier>, hand : Arc<Mutex<Vec<Card>>>, _field : Vec<Card>) {
 
     let mut turn_no: i32 = 0;
+    let mut played_cards: Vec<Card> = Vec::with_capacity(9);
     println!("Player {player_id}:  Turn is {turn_no}");
     
     while hand.lock().unwrap().is_empty() == false {
@@ -15,9 +16,10 @@ pub fn create_player(player_id : usize, turn_barrier: Arc<Barrier>, hand : Arc<M
         let mut player_hand: std::sync::MutexGuard<'_, Vec<Card>> = hand.lock().unwrap();
 
         let card = player_hand.pop().unwrap();
-
+        println!("Player {player_id} on round {turn_no}: Player {player_id} plays {card}");
+        
+        played_cards.push(card);
         turn_no = turn_no+1;
-        println!("Player {player_id} on round {turn_no}: Player {player_id} plays {card}")
     }
 
     println!("Player {player_id} thread is exiting");
